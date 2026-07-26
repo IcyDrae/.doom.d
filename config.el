9c09c529f62fa15c1e9023e07510428614373629
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2026-07-26 21:05:36 +0200

    add RSS support and customize dashboard banner ASCII art

--- config.el
+++ config.el
@@ -3,15 +3,15 @@
 
 (setq doom-font (font-spec
                  :family "Ioskeley Mono"
-                 :size 20)
+                 :size 22)
 
       doom-variable-pitch-font (font-spec
                                 :family "Ioskeley Mono"
-                                :size 20)
+                                :size 22)
 
       doom-big-font (font-spec
                      :family "Ioskeley Mono"
-                     :size 22))
+                     :size 24))
 
 ;; Maximize on start
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
@@ -28,6 +28,16 @@
 
 ;; Save place in document when reopening files
 (save-place-mode 1)
+
+;; Dashboard banner customization
+(setq +dashboard-ascii-banner-fn
+      (lambda ()
+        "    __               
+   / / __ __ _ _   _ 
+  / / '__/ _` | | | |
+ / /| | | (_| | |_| |
+/_/ |_|  \__,_|\__, |
+               |___/ "))
 
 ;; Add trailing lines
 (after! ws-butler
@@ -80,3 +90,51 @@
 
       :desc "Continue org timer"
       "o t c" #'org-timer-pause-or-continue)
+
+(after! elfeed
+  ;; RSS feeds
+  (setq elfeed-feeds
+        '(
+          ;; --------------------
+          ;; News
+          ;; --------------------
+          ("https://www.aljazeera.com/xml/rss/all.xml" news world aljazeera)
+          ("https://feeds.bbci.co.uk/news/rss.xml" news bbc)
+          ("https://feeds.bbci.co.uk/news/world/rss.xml" news world bbc)
+
+          ;; --------------------
+          ;; Reddit
+          ;; --------------------
+          ("https://www.reddit.com/r/linux/.rss" reddit linux)
+          ("https://www.reddit.com/r/commandline/.rss" reddit commandline)
+          ("https://www.reddit.com/r/emacs/.rss" reddit emacs)
+
+          ;; --------------------
+          ;; Linux & Open Source
+          ;; --------------------
+          ("https://hackaday.com/blog/feed/" hackaday hardware linux)
+          ("https://opensource.com/feed" opensource linux)
+          ("https://www.techrepublic.com/rssfeeds/topic/open-source/" techrepublic linux opensource)
+          ("https://www.phoronix.com/rss.php" linux phoronix)
+          ("https://planet.kde.org/rss20.xml" linux kde)
+
+          ;; --------------------
+          ;; Programming
+          ;; --------------------
+          ("https://hnrss.org/frontpage" programming hackernews)
+          ("https://lobste.rs/rss" programming)
+
+          ;; --------------------
+          ;; Emacs
+          ;; --------------------
+          ("https://planet.emacslife.com/atom.xml" emacs)
+
+          ;; --------------------
+          ;; Security
+          ;; --------------------
+          ("https://feeds.feedburner.com/TheHackersNews" security))))
+
+(custom-set-faces!
+  '(elfeed-search-feed-face
+    :foreground "#ffffff"
+    :weight bold))
